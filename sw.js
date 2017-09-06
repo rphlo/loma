41a24650e9f8d23cd14b45f89754951a4e914fa1
Raphaël Stefanini <rphl@rphl.net>
2017-09-06 11:10:40 +0300

    Add offline mode

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(e){
+ e.waitUntil(
+   caches.open('lomalaskuri').then(function(cache){
+     return cache.addAll([
+       'index.html',
+       'holiday.jpg',
+       'work.jpg',
+     ]);
+   })
+ );
+});
