976b91779cbf71ac8d9569075ae741322153958f
Raphaël Stefanini <rphlo@users.noreply.github.com>
2017-09-19 20:22:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(e){
  e.waitUntil(
    caches.open('lomalaskuri').then(function(cache){
      return cache.addAll([
-       '/loma/',
-       'index.html',
-       'holiday.jpg',
-       'work.jpg',
+       './',
+       './index.html',
+       './holiday.jpg',
+       './work.jpg',
      ]);
    })
  );
