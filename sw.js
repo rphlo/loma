79b0563b51812cfe1a8dbdb3d7338492e75e4b21
Raphaël Stefanini <rphlo@users.noreply.github.com>
2018-02-21 11:05:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
 self.addEventListener('install', function(e){
+ var timeStamp = Date.now();
  e.waitUntil(
    caches.open('lomalaskuri').then(function(cache){
      return cache.addAll([
        './',
-       './index.html',
-       './holiday.jpg',
-       './work.jpg',
-     ]);
+       './index.html?ts='+timeStamp,
+       './holiday.jpg?ts='+timeStamp,
+       './work.jpg?ts='+timeStamp
+     ]).then(function(){self.skipWaiting()});
    })
  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true}).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
 });
